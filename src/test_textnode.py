e6eb5691ec998ec008a29b1a4652e139343cf5c7
Justin Meeks <jt@mac.mynetworksettings.com>
2025-02-10 22:48:17 -0500

    textnode to html func

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,13 +1,13 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
 	def test_eq(self):
-		 node = TextNode("This is a text node", TextType.BOLD)
-		 node2 = TextNode("This is a text node", TextType.BOLD)
-		 self.assertEqual(node, node2)
+		node = TextNode("This is a text node", TextType.BOLD)
+		node2 = TextNode("This is a text node", TextType.BOLD)
+		self.assertEqual(node, node2)
 	def test_url(self):
 		node = TextNode("blank", TextType.BOLD, url="monkey.com")
 		node2 = TextNode("blank", TextType.BOLD)
@@ -20,6 +20,23 @@ class TestTextNode(unittest.TestCase):
 		node = TextNode("blank",TextType.BOLD)
 		node2 = TextNode("blank",TextType.BOLD)
 		self.assertEqual(node, node2)
+	def test_text_type(self):
+		text_node = TextNode("Hello, World", TextType.TEXT)
+		html_node = text_node_to_html_node(text_node)
+		assert html_node.to_html() == "Hello, World", f"Expected 'Hello, World', but got {html_node.to_html()}"
+	def test_bold_type(self):
+		text_node = TextNode("Bold Text", TextType.BOLD)
+		html_node = text_node_to_html_node(text_node)
+		assert html_node.to_html() == "<b>Bold Text</b>", f"Expected '<b>Bold Text</b>', but got {html_node.to_html()}"
+	def test_link_type_missing_url(self):
+		text_node = TextNode("Click Here", TextType.LINK)
+		try:
+			text_node_to_html_node(text_node)
+		except ValueError as e:
+			assert str(e) == "URL is required for LINK type", f"Expected 'URL is required for LINK type', but got {str(e)}"
+
+
+
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = 'text'
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -23,3 +24,23 @@ class TextNode:
 
 	def __repr__(self):
 		return f"TextNode(text={repr(self.text)}, text_type={repr(self.text_type)}, url={repr(self.url)})"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(value=text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        if not text_node.url:
+            raise ValueError("URL is required for LINK type")
+        return LeafNode(tag="a", value=text_node.text, href=text_node.url)
+    elif text_node.text_type == TextType.IMAGE:
+        if not text_node.url:
+            raise ValueError("URL is required for IMAGE type")
+        return LeafNode(tag="img", value="", src=text_node.url, alt=text_node.text)  # Using `text` as alt
+    else:
+        raise ValueError(f"Unknown text type: {text_node.text_type}")
